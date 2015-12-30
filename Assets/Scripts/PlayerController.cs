e95988c47d582dc3f61b1384a5bd82d40a3c3f1c
Nikos Katsos <nikoskeyz@gmail.com>
2015-12-31 01:10:46 +0200

    score system added
    ready for points on screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	private static int NUMBER_OF_CUBES = 19;
 
+	public float speed;
+	public Text CountText;
 	private Rigidbody rigidBody;
+	private int count;
 
 	void Start() 
 	{
+		count = 0;
+		SetCountText ();
 		rigidBody = GetComponent<Rigidbody> ();
 	}
 
@@ -20,5 +26,23 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rigidBody.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() 
+	{
+		if ( count == NUMBER_OF_CUBES )
+			CountText.text = "You WON !!!";
+		else
+			CountText.text = "Cubes: " + count.ToString ();
 	}
 }
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	private int x, y, z;
+
+	void Start() {
+		x = y = z = 45;
+	}
+
+	void Update () {
+		transform.Rotate (new Vector3 (x, y, z) * Time.deltaTime);
+	}
+}
